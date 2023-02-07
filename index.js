5a8a0cd33d6841113c4819f78c7d946ce16ce472
tpmy <tomitoboulanger@hotmail.com>
2023-02-06 23:24:05 -0300

    SegundaEntrega

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const fs = require('fs')
+
 class ProductManager {
     
 
     constructor() {
-        this.productos = []
+         this.productos = []
     }
 
     getProducts(){
         return this.productos
     }
-
-    addProduct(title, description, price, thumbnail, code, stock, id){
+    
+    async addProduct(title, description, price, thumbnail, code, stock, id){
         const CadaProducto = {
             title,
             description,
@@ -24,12 +26,17 @@ class ProductManager {
          } else {
             CadaProducto.id = this.productos[this.productos.length - 1].id + 1;
          }
-        this.productos.push(CadaProducto)
+    const comoJson = JSON.stringify(CadaProducto)
+
+    fs.promises.writeFile('Productos.json', comoJson)
+
+    const ProductosLeidos = JSON.parse(await fs.promises.readFile('Productos.json', 'utf-8'))
+            this.productos.push(CadaProducto)
+            console.log(ProductosLeidos);
     }
 
-
-     getProductbyId(idCadaProducto) {
-         const elegirProducto = this.productos.find(e => e.id === idCadaProducto);
+     async getProductbyId(idCadaProducto) {
+         const elegirProducto = await this.productos.find(e => e.id === idCadaProducto);
          if (!elegirProducto) {
              throw new Error("No encontrado");
          }
@@ -38,11 +45,18 @@ class ProductManager {
           }
         this.productos.push(nuevoProducto);
      }
+
+     async deleteProduct(idCadaProducto) {
+        const borrarProducto = await this.productos.find(e => e.id === idCadaProducto);
+        if (borrarProducto) {
+            fs.rmSync
+        }
+     }
+     
 }
 
 const Productor = new ProductManager()
 Productor.addProduct('Evento1','Lugar1',2.5, ' asdasd' , 'asas', 1)
-Productor.addProduct('Evento1','Lugar1',2.5, ' asdasd' , 'asas', 1)
-Productor.getProductbyId(1)
+Productor.deleteProduct(1)
 console.log(Productor.getProducts())
-// console.log(Productor)
+console.log(Productor);
